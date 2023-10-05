f5373bab960769b2addaff860e7d33d42bb40996
abrartariqulislam <islamtariqul010@gmail.com>
2023-10-05 10:10:18 +0600

    update

--- routes.js
+++ routes.js
@@ -8,11 +8,13 @@
 // Dependencies
 const { sampleHandler } = require("./handlers/routesHandlers/sampleHndler");
 const  {userHandler} = require("./handlers/routesHandlers/userHandler")
+const  {tokenHandler} = require("./handlers/routesHandlers/tokenHandler")
 
 // routes Object - Module Scaffolding
 const routes = {
   sample: sampleHandler,
   user: userHandler,
+  token:tokenHandler,
 };
 
 // exports
